70574abb92d377a2b247cc49853dec64bd184f8d
danilo di cuia <dicuia@google.com>
2022-07-01 18:01:55 +0200

    added bounds

--- textToPoints/sketch.js
+++ textToPoints/sketch.js
@@ -1,12 +1,20 @@
 let pts
 let caslon
+let img
+let gr
 
+const grw = 300
+const grh = 116
+const fishPoints = []
 function preload() {
   caslon = loadFont("manofa.ttf")
+  img = loadImage("fish.png")
 }
 
+const s = 0.3
+
 function setup() {
-  createCanvas(600, 600)
+  createCanvas(window.innerWidth, window.innerHeight)
 
   pts = caslon.textToPoints("ZERRO", 0, 0, 120, {
     sampleFactor: 0.12,
@@ -20,11 +28,52 @@ function setup() {
       })
     )
   )
+  fill(255)
+  background(150)
+  image(img, 0, 0, grw, grh)
+
+  const int = 5
+
+  console.log("analyzing fish...")
+  for (let j = 0; j <= grw; j += int) {
+    for (let k = 0; k <= grh; k += int) {
+      const p = get(j, k)
+      if (p[0] == 0) {
+        fishPoints.push({ x: j, y: k })
+      }
+    }
+  }
+  while (fishPoints.length > pts.length) {
+    fishPoints.splice(round(random(fishPoints.length - 1)), 1)
+  }
+  console.log(fishPoints.length)
+  console.log(
+    JSON.stringify(
+      fishPoints.map((d) => {
+        return { x: d.x, y: d.y }
+      })
+    )
+  )
+
+  // for (let i = 0; i < grw * grh; i += 10) {
+  //   if (gr.pixels[i] < 255) {
+  //     fishPoints.push({ x: i % grw, y: floor(i / grh) })
+  //   }
+  // }
+  console.log(fishPoints)
+
+  noStroke()
+  fill(255, 0, 0)
+  for (let i = 0; i < fishPoints.length; i++) {
+    const { x, y } = fishPoints[i]
+    const n = (noise(x, y) * 2 - 1) * 10
+    ellipse(fishPoints[i].x + n, fishPoints[i].y + n, 5, 5)
+  }
   // for(let i =0; i<pts.length;
 }
 
-function draw() {
-  background(220)
+function drawa() {
+  background(225)
 
   translate(20, 140)
   fill(0)
@@ -32,4 +81,6 @@ function draw() {
   for (let i = 0; i < pts.length; i++) {
     ellipse(pts[i].x, pts[i].y, 2, 2)
   }
+
+  fill(255)
 }
